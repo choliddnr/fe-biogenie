89c223b83da4d99582f58c42661bb3ba46b9cd6e
choliddnr <cholid.dnr@gmail.com>
2026-01-26 19:58:26 +0700

    add product page

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'aboutus.html'),
+        product: resolve(__dirname, 'product.html'),
       },
     },
   },
